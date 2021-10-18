09b36f7ed6c34f2798e06813c90107a787e147d9
ShubhamPV <shubhamvadnere5@gmail.com>
2021-10-18 20:10:47 +0530

    [ShubhamPv] CreationBST

---
+++ HashTable/BST.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable
+{
+
+    class BinarySearchTree<T> where T : IComparable<T>
+    {
+        public T NodeData { get; set; }
+        public BinarySearchTree<T> LeftTree { get; set; }
+        public BinarySearchTree<T> RightTree { get; set; }
+        public static int leftcCount;
+        public static int righyCount;
+        /// <summary>
+        /// Assigning the initiation
+        /// </summary>
+        /// <param name="nodeData"></param>
+        public BinarySearchTree(T nodeData)
+        {
+            this.NodeData = nodeData;
+            this.LeftTree = null;
+            this.RightTree = null;
+        }
+        int leftCount = 0, rightCount = 0;
+        bool result = false;
+        public void Insert(T item) //Insert into the Tree
+        {
+            T currentNodeValue = this.NodeData;
+            if ((currentNodeValue.CompareTo(item) > 0))
+            {
+                if (this.LeftTree == null)
+                    this.LeftTree = new BinarySearchTree<T>(item);
+                else
+                    this.LeftTree.Insert(item); //Insert onto the Left side of Tree
+            }
+            else
+            {
+                if (this.RightTree == null)
+                    this.RightTree = new BinarySearchTree<T>(item);
+                else
+                    this.RightTree.Insert(item); //Insert into Right side of Tree
+            }
+        }
+        public void Display() //Display of Binary Search Tree.
+        {
+
+            if (this.LeftTree != null)
+            {
+                leftCount++;
+                this.LeftTree.Display();
+            }
+            Console.WriteLine(this.NodeData.ToString());
+            if (this.RightTree != null)
+            {
+                rightCount++;
+                this.RightTree.Display();
+            }
+        }
+
+    }
+}
--- HashTable/Program.cs
+++ HashTable/Program.cs
@@ -1,5 +1,4 @@
-﻿using HashTable;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,20 +10,11 @@ namespace HashTable
     {
         static void Main(string[] args)
         {
-            MyMapNode<string, string> hash = new MyMapNode<string, string>(6);
-            hash.Add("1", "to");
-            hash.Add("2", "be");
-            hash.Add("3", "or");
-            hash.Add("4", "not");
-            hash.Add("5", "to");
-            hash.Add("6", "be");
-            Console.WriteLine("Displaying elements of hashtable before removing node");
-            hash.Display();
-            hash.Remove("4");
-            Console.WriteLine("Displaying elements of hashtable after removing node");
-            hash.Display();
-
-
+            Console.WriteLine("Binary Search Tree");
+            BinarySearchTree<int> binaryTree = new BinarySearchTree<int>(56);
+            binaryTree.Insert(30);
+            binaryTree.Insert(70);
+            binaryTree.Display();
             Console.ReadLine();
         }
     }
